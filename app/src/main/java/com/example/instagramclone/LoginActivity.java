415de1d1b1d68a360ea9c13349936ae8531ef942
Phinehas Fuachie <phinehas@fb.com>
2022-06-14 13:41:21 -0700

    Add Login feature

---
+++ app/src/main/java/com/example/instagramclone/LoginActivity.java
@@ -0,0 +1,78 @@
+package com.example.instagramclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText etUsername;
+    EditText etPassword;
+    Button loginButton;
+    ImageView ivLogo;
+    public static final String TAG = "LoginACtivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        etPassword = findViewById(R.id.editTextTextPersonName2);
+        etUsername = findViewById(R.id.editTextTextPersonName);
+        loginButton = findViewById(R.id.button);
+        ivLogo = findViewById(R.id.imageView);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Onclick login button");
+                String username = etUsername.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username, password);
+            }
+        });
+
+
+
+
+
+
+    }
+
+    private void loginUser(String username, String password) {
+        Log.i(TAG, "Attempting to login user" + username);
+
+        ParseUser.logInInBackground(username,password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                Log.d(TAG, "tttt"+ e);
+                if (e != null){
+                    Log.e(TAG, "Issue with login");
+                }
+                else {
+                goMainActivity();
+                Toast.makeText(LoginActivity.this, "Success", Toast.LENGTH_SHORT).show();
+
+            }}
+        });
+
+    }
+
+    private void goMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+}
---
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -0,0 +1,20 @@
+package com.example.instagramclone;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApplication extends Application {
+    public void onCreate() {
+        super.onCreate();
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .
+                applicationId("zV0kEDFHheN7FXpsXkwNNgkfFiDV2pInPlE9JuH3")
+                .clientKey("PlBsZvnzBk4Gt1kEsSFL9eCVAbVQaWuT2mK4UkpI")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+
+}
